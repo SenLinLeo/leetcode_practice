480cb4ab6363b98f1a158358b1aee01df3e5622d
faberli <faberli@tencent.com>
2024-01-21 14:31:00 +0800

    [upd]LeetCode 287. 寻找重复数

--- leetcode/287.go
+++ leetcode/287.go
@@ -1,4 +1,4 @@
-// Package main 2023/10/21
+// Package main 2023/10/17
 package main
 
 import (
@@ -6,10 +6,11 @@ import (
 )
 
 /**
+题目：287. 寻找重复数
+https://leetcode.cn/problems/find-the-duplicate-number/description/
 给定一个包含 n + 1 个整数的数组 nums ，其数字都在 [1, n] 范围内（包括 1 和 n），可知至少存在一个重复的整数。
 假设 nums 只有 一个重复的整数 ，返回 这个重复的数 。
 你设计的解决方案必须 不修改 数组 nums 且只用常量级 O(1) 的额外空间。
-https://leetcode.cn/problems/find-the-duplicate-number/solutions/261119/xun-zhao-zhong-fu-shu-by-leetcode-solution/?envType=study-plan-v2&envId=top-100-liked
 **/
 func findDuplicate(nums []int) int {
 	if len(nums) == 0 {
@@ -21,7 +22,6 @@ func findDuplicate(nums []int) int {
 	}
 
 	slow = 0
-	// a=(k−1)L+(L−b)=(k−1)L+c
 	for slow != fast {
 		slow = nums[slow]
 		fast = nums[fast]
@@ -30,7 +30,6 @@ func findDuplicate(nums []int) int {
 }
 
 func main() {
-	test1 := []int{2, 5, 9, 6, 9, 3, 8, 9, 7, 1}
-	result1 := findDuplicate(test1)
-	fmt.Println(result1)
+	nums1 := []int{1, 3, 3, 2}
+	fmt.Println(findDuplicate(nums1))
 }
