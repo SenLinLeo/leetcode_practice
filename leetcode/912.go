cc3d1c07ed8bc000a2201fc6063213af126e64e5
faberli@tencent.com <faberli@tencent.com>
2024-01-22 19:19:20 +0800

    [upd]LeetCode 912. 排序数组

---
+++ leetcode/912.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+/**
+912. 排序数组
+https://leetcode.cn/problems/sort-an-array/description/
+**/
+func sortArray(nums []int) []int {
+	QuickSort(nums, 0, len(nums)-1)
+	return nums
+}
+
+func partition(list []int, low, high int) int {
+	pivot := list[low] //导致 low 位置值为空
+	for low < high {
+		//high指针值 >= pivot high指针左移
+		for low < high && pivot <= list[high] {
+			high--
+		}
+		//填补low位置空值
+		//high指针值 < pivot high值 移到low位置
+		//high 位置值空
+		list[low] = list[high]
+		//low指针值 <= pivot low指针右移
+		for low < high && pivot >= list[low] {
+			low++
+		}
+		//填补high位置空值
+		//low指针值 > pivot low值 移到high位置
+		//low位置值空
+		list[high] = list[low]
+	}
+	//pivot 填补 low位置的空值
+	list[low] = pivot
+	return low
+}
+
+func QuickSort(list []int, low, high int) {
+	if high > low {
+		//位置划分
+		pivot := partition(list, low, high)
+		//左边部分排序
+		QuickSort(list, low, pivot-1)
+		//右边排序
+		QuickSort(list, pivot+1, high)
+	}
+}
+
+func main() {
+	list := []int{2, 44, 4, 8, 33, 1, 22, -11, 6, 34, 55, 54, 9}
+	listSort := sortArray(list)
+	fmt.Println(listSort)
+}
