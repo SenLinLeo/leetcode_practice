3c943b7afd739330e023e4fe9fd3a60e2cf465a7
SenLinLeo <faberli@qq.com>
2024-01-23 02:36:11 +0800

    [upd]LeetCode LCR 095. 最长公共子序列 [动态规划]

--- leetcode-go/lcr095.go
+++ leetcode-go/lcr095.go
@@ -6,12 +6,12 @@ import (
 )
 
 /** LCR 095. 最长公共子序列
-// https://leetcode.cn/problems/qJnOS7/description/
-// 给定两个字符串 text1 和 text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。
-//输入：text1 = "abcde", text2 = "ace"
-//输出：3
-//解释：最长公共子序列是 "ace" ，它的长度为 3 。
-// 时间空间复杂度O(n)
+https://leetcode.cn/problems/qJnOS7/description/
+给定两个字符串 text1 和 text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。
+输入：text1 = "abcde", text2 = "ace"
+输出：3
+解释：最长公共子序列是 "ace" ，它的长度为 3 。
+时间空间复杂度O(n)
 */
 func longestCommonSubsequence(text1 string, text2 string) int {
 	n, m := len(text1), len(text2)
