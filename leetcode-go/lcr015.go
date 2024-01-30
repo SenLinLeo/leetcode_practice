d012ccbadeaab1748c63667f0e0241b41e959fc6
SenLinLeo <faberli@qq.com>
2024-01-30 11:03:36 +0800

    [upd]LeetCode LCR 015. 找到字符串中所有字母异位词

--- leetcode-go/lcr015.go
+++ leetcode-go/lcr015.go
@@ -3,54 +3,20 @@ package main
 
 import (
 	"fmt"
-	"reflect"
-	"sort"
 )
 
-// 给定一个字符串 s 和一个非空字符串 p，找到 s 中所有是 p 的字母异位词的子串，返回这些子串的起始索引
-func findAnagramsN(s string, p string) []int {
-	res := make([]int, 0)
-	pCount := make(map[byte]int)
-	for i := 0; i < len(p); i++ {
-		pCount[p[i]]++
-	}
-	sCount := make(map[byte]int)
-	for i := 0; i < len(s); i++ {
-		sCount[s[i]]++
-		if i >= len(p) {
-			sCount[s[i-len(p)]]--
-			if sCount[s[i-len(p)]] == 0 {
-				delete(sCount, s[i-len(p)])
-			}
-		}
-		if reflect.DeepEqual(sCount, pCount) {
-			res = append(res, i-len(p)+1)
-		}
-	}
-	return res
-}
+/**
+LCR 015. 找到字符串中所有字母异位词
+https://leetcode.cn/problems/VabMRr/description/
+给定一个字符串 s 和一个非空字符串 p，找到 s 中所有是 p 的字母异位词的子串，返回这些子串的起始索引
 
-// findAnagramsLogN 可以使用滑动窗口和排序来实现
-func findAnagramsLogN(s string, p string) []int {
-	res := make([]int, 0)
-	pSort := sortString(p)
-	for i := 0; i <= len(s)-len(p); i++ {
-		sSort := sortString(s[i : i+len(p)])
-		if sSort == pSort {
-			res = append(res, i)
-		}
-	}
-	return res
-}
-
-func sortString(s string) string {
-	sBytes := []byte(s)
-	sort.Slice(sBytes, func(i, j int) bool {
-		return sBytes[i] < sBytes[j]
-	})
-	return string(sBytes)
-}
-
+示例 1：
+输入: s = "cbaebabacd", p = "abc"
+输出: [0,6]
+解释:
+起始索引等于 0 的子串是 "cba", 它是 "abc" 的变位词。
+起始索引等于 6 的子串是 "bac", 它是 "abc" 的变位词。
+**/
 // 滑动窗口
 func findAnagrams(s, p string) (ans []int) {
 	sLen, pLen := len(s), len(p)
@@ -78,7 +44,5 @@ func findAnagrams(s, p string) (ans []int) {
 }
 
 func main() {
-	fmt.Println(findAnagramsLogN("cbaebabacd", "abc"))
-	fmt.Println(findAnagramsN("cbaebabacd", "abc"))
 	fmt.Println(findAnagrams("cbaebabacd", "abc"))
 }
