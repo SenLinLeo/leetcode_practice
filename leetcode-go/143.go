f38d596568bd4ae2bc1fed988a65d6b5fe0a19fc
SenLinLeo <faberli@qq.com>
2024-02-04 15:49:37 +0800

    [upd]LeetCode 143. 重排链表

---
+++ leetcode-go/143.go
@@ -0,0 +1,63 @@
+package main
+
+/**
+143. 重排链表
+
+给定一个单链表 L 的头节点 head ，单链表 L 表示为：
+L0 → L1 → … → Ln - 1 → Ln
+请将其重新排列后变为：
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+要重排的链表为：1 -> 2 -> 3 -> 4 -> 5 -> 6
+
+思路：
+第一步，将链表平均分成两半
+1 -> 2 -> 3
+4 -> 5 -> 6
+
+第二步，将第二个链表逆序
+1 -> 2 -> 3
+6 -> 5 -> 4
+
+第三步，依次连接两个链表
+1 -> 6 -> 2 -> 5 -> 3 -> 4
+**/
+func reorderList(head *ListNode) {
+	mid := middleNode(head)
+	head2 := reverseList(mid)
+	for head2.Next != nil {
+		nxt := head.Next
+		nxt2 := head2.Next
+		head.Next = head2
+		head2.Next = nxt
+		head = nxt
+		head2 = nxt2
+	}
+}
+
+// 876. 链表的中间结点
+func middleNode(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	return slow
+}
+
+// 206. 反转链表
+func reverseList(head *ListNode) *ListNode {
+	var pre, cur *ListNode = nil, head
+	for cur != nil {
+		nxt := cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = nxt
+	}
+	return pre
+}
+
+func main() {
+
+}
