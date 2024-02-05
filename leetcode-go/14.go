db6def44ab3c565a129b7b73650924b87374aadf
SenLinLeo <faberli@qq.com>
2024-02-05 15:18:44 +0800

    [upd]LeetCode 14. 最长公共前缀

---
+++ leetcode-go/14.go
@@ -0,0 +1,30 @@
+package main
+
+/**
+14. 最长公共前缀
+编写一个函数来查找字符串数组中的最长公共前缀。
+如果不存在公共前缀，返回空字符串 ""。
+
+示例 1：
+输入：strs = ["flower","flow","flight"]
+输出："fl"
+时间复杂度: O(mn)
+空间复杂度：O(1)
+**/
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+	for i := 0; i < len(strs[0]); i++ {
+		for j := 1; j < len(strs); j++ {
+			if i == len(strs[j]) || strs[j][i] != strs[0][i] {
+				return strs[0][:i]
+			}
+		}
+	}
+	return strs[0]
+}
+
+func main() {
+
+}
