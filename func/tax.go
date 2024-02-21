f57cb1380b2e1d5feb9015fd89fa526aba1c4673
SenLinLeo <faberli@qq.com>
2024-02-21 11:07:50 +0800

    [upd]test

--- func/tax.go
+++ func/tax.go
@@ -13,11 +13,13 @@ func getYear() string {
 	return fmt.Sprintf("%0.2f", afterTax)
 }
 
+// https://www.gerensuodeshui.cn/city/%E6%B7%B1%E5%9C%B3.html
 func getMonth() string {
 	// 调整项
 	otherAmount := 10340.00
 	otherTax := otherAmount * 0.03
 	resTax := 1739.37 - otherTax
+	resTax = 918.77
 	resAmount := 29400 + 200 - resTax - 6771.95
 
 	return fmt.Sprintf("%0.2f", resAmount)
@@ -25,7 +27,5 @@ func getMonth() string {
 
 func main() {
 	fmt.Println("getYear():", getYear(), "--", 13494.81+92610.00)            // 92610.00
-	fmt.Println("getMonth():", getMonth(), "--", 13494.81+92610.00+21398.88) // 21398.88
-	// 13494.81
-	fmt.Println(48414.81 - 34920.00)
+	fmt.Println("getMonth():", getMonth(), "--", 13494.81+92610.00+21909.28) // 21398.88 ->21909.28
 }
