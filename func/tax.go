f8747f9bd9eb69cb74555dba49184c84ebb1a592
SenLinLeo <faberli@qq.com>
2024-03-25 00:56:47 +0800

    [upd]common

--- func/tax.go
+++ func/tax.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"github.com/dustin/go-humanize"
 )
 
@@ -11,9 +10,9 @@ import (
 累计应纳税所得额=月薪*月数-社保公积金*月数-专项附加扣除*月数-起征点*月数
 当月个税=累计应纳税所得额*年度税率-年度扣除数-当年度累计已缴纳个税
 **/
-func getYear() string {
+func getYear() float64 {
 	afterTax := 92820.00
-	return fmt.Sprintf("%v", humanize.Commaf(afterTax))
+	return afterTax
 }
 
 // https://www.gerensuodeshui.cn/city/%E6%B7%B1%E5%9C%B3.html
@@ -25,15 +24,15 @@ func getMonth() float64 {
 }
 
 func main() {
-	startAmount := 13494.81
-	fYear, _ := strconv.ParseFloat(getYear(), 64)
-	fmt.Println("第1笔getMonth():", humanize.Commaf(getMonth()), " 余额: ", humanize.Commaf(startAmount+fYear))
-	fmt.Println("第2笔getYear():", getYear(), " 余额: ", humanize.Commaf(startAmount+fYear+getMonth()))
+	startAmount := 1494.02
+	fYear := humanize.Commaf(getYear())
+	fMonth := humanize.Commaf(getMonth())
+	fmt.Println("第1笔Mon:", fMonth, "\t余额: ", humanize.Commaf(startAmount+getYear()+getMonth()))
+	fmt.Println("第2笔Yea:", fYear, "\t余额: ", humanize.Commaf(startAmount+getYear()))
 	/**
-	第1笔getMonth(): 21,758.59  余额:  13,494.81
-	第2笔getYear(): 92,820  余额:  35,253.4
+	第1笔Mon: 21,758.59     余额:  116,072.61
+	第2笔Yea: 92,820        余额:  94,314.02
 	**/
-
 	fmt.Println(humanize.Commaf(102900.00))
 	fmt.Println(humanize.Commaf(102900.00 - 29400.00 - 29400.00))
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module go_practice
+
+go 1.21.8
+
+require github.com/dustin/go-humanize v1.0.1
